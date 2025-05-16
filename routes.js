828b224f5ff209e6bc4adc948a268d6bfb1299da
Nagothu <sai.p.nagothu@accenture.com>
2025-05-16 11:34:53 +0530

    Updated server.js and routes.js logic

--- routes.js
+++ routes.js
@@ -1,3 +1,40 @@
+// import {
+//   getAllBooks,
+//   getBookDetails,
+//   addBook,
+//   updateBookDetails,
+//   deleteBook
+// } from './book_handler.js';
+
+// export const routes = [
+//   {
+//     path: /^\/books$/,
+//     method: 'GET',
+//     handler: getAllBooks,
+//   },
+//   {
+//     path: /^\/books\/(\d+)$/,
+//     method: 'GET',
+//     handler: getBookDetails,
+//   },
+//   {
+//     path: /^\/books\/(\d+)$/,
+//     method: 'PUT',
+//     handler: updateBookDetails,
+//   },
+//   {
+//     path: /^\/books$/,
+//     method: 'POST',
+//     handler: addBook,
+//   },
+//   {
+//     path: /^\/books\/(\d+)$/,
+//     method: 'DELETE',
+//     handler: deleteBook,
+//   },
+// ];
+
+
 import {
   getAllBooks,
   getBookDetails,
@@ -6,31 +43,30 @@ import {
   deleteBook
 } from './book_handler.js';
 
-export const routes = [
-  {
-    path: /^\/books$/,
-    method: 'GET',
-    handler: getAllBooks,
-  },
-  {
-    path: /^\/books\/(\d+)$/,
-    method: 'GET',
-    handler: getBookDetails,
-  },
-  {
-    path: /^\/books\/(\d+)$/,
-    method: 'PUT',
-    handler: updateBookDetails,
-  },
-  {
-    path: /^\/books$/,
-    method: 'POST',
-    handler: addBook,
-  },
-  {
-    path: /^\/books\/(\d+)$/,
-    method: 'DELETE',
-    handler: deleteBook,
-  },
-];
 
+// routeMap is a JavaScript object (acting like a hashmap/dictionary) where:
+// The keys are HTTP methods: 'GET', 'POST', 'PUT', 'DELETE'
+// Each route definition is an object with:
+
+
+// path: a regular expression to match against the incoming request’s URL
+
+
+// handler: the function to execute when this path is matched
+
+
+export const routeMap = {
+  GET: [
+    { path: /^\/books$/, handler: getAllBooks },
+    { path: /^\/books\/(\d+)$/, handler: getBookDetails }
+  ],
+  POST: [
+    { path: /^\/books$/, handler: addBook }
+  ],
+  PUT: [
+    { path: /^\/books\/(\d+)$/, handler: updateBookDetails }
+  ],
+  DELETE: [
+    { path: /^\/books\/(\d+)$/, handler: deleteBook }
+  ]
+};
--- server.js
+++ server.js
@@ -1,24 +1,58 @@
+// import http from 'http';
+// import { routes } from './routes.js';
+
+// const server = http.createServer((req, res) => {
+//   const matchedRoute = routes.find(route =>
+//     req.method === route.method && route.path.test(req.url)
+//   );
+
+//   if (matchedRoute) {
+//     const match = req.url.match(matchedRoute.path);
+//     const id = match && match[1] ? parseInt(match[1]) : null;
+
+//     matchedRoute.handler(req, res, id);
+//   } else {
+//     res.writeHead(404, { 'Content-Type': 'text/plain' });
+//     res.end('Route not found');
+//   }
+// });
+
+// server.listen(4444, () => {
+//   console.log('Books API server is running on port 4444');
+// });
+
+
+
 import http from 'http';
-import { routes } from './routes.js';
+import { routeMap } from './routes.js';
+
 
 const server = http.createServer((req, res) => {
-  const matchedRoute = routes.find(route =>
-    req.method === route.method && route.path.test(req.url)
-  );
+  const routesForMethod = routeMap[req.method];
 
-  if (matchedRoute) {
-    const match = req.url.match(matchedRoute.path);
-    const id = match && match[1] ? parseInt(match[1]) : null;
 
-    matchedRoute.handler(req, res, id);
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Route not found');
+  if (routesForMethod) {
+    const matchedRoute = routesForMethod.find(route =>
+      route.path.test(req.url)
+    );
+
+
+    if (matchedRoute) {
+      const match = req.url.match(matchedRoute.path);
+      const id = match && match[1] ? parseInt(match[1]) : null;
+
+
+      matchedRoute.handler(req, res, id);
+      return;
+    }
   }
+
+
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.end('Route not found');
 });
+
 
 server.listen(4444, () => {
   console.log('Books API server is running on port 4444');
 });
-
-
